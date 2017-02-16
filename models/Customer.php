de784b550ecc50eac1bdcaedef75f662230e55ef
Alexandre Gadiou <alexandregadiou44@icloud.com>
2017-02-16 20:13:15 +0100

    add customer model

---
+++ models/Customer.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Algad\Hotel\Models;
+
+use RainLab\User\Models\User;
+
+/**
+ * Model
+ */
+class Customer extends User
+{
+
+    public $rules = [
+        'email' => 'required|between:6,255|email|unique:users',
+        'password' => 'required:create|between:4,255|confirmed',
+        'password_confirmation' => 'required_with:password|between:4,255'
+    ];
+
+}
