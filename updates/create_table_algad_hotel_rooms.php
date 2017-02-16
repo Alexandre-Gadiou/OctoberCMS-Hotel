ffa1eecdbe35c17ae99cfe92d788568c99ad0def
Alexandre Gadiou <alexandregadiou44@icloud.com>
2017-02-16 15:30:50 +0100

    change room number to string

--- updates/create_table_algad_hotel_rooms.php
+++ updates/create_table_algad_hotel_rooms.php
@@ -1,10 +1,13 @@
-<?php namespace Algad\Hotel\Updates;
+<?php
+
+namespace Algad\Hotel\Updates;
 
 use Schema;
 use October\Rain\Database\Updates\Migration;
 
 class CreateTableAlgadHotelRooms extends Migration
 {
+
     public function up()
     {
         Schema::create('algad_hotel_rooms', function($table)
@@ -13,13 +16,14 @@ class CreateTableAlgadHotelRooms extends Migration
             $table->increments('id');
             $table->string('title', 255)->nullable();
             $table->string('slug', 255)->nullable();
-            $table->integer('number')->nullable();
+            $table->string('number', 11)->nullable();
             $table->boolean('is_available');
         });
     }
-    
+
     public function down()
     {
         Schema::dropIfExists('algad_hotel_rooms');
     }
+
 }
