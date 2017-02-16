b5c00cddc12fd4e493020e87ff73fd380913fa9f
Alexandre Gadiou <alexandregadiou44@icloud.com>
2017-02-16 14:39:10 +0100

    init hotel octoberCMS plugin

---
+++ lang/en/lang.php
@@ -0,0 +1,20 @@
+<?php return [
+
+    'plugin' => [
+        'name' => 'Hotel',
+        'description' => 'room, customer, bill management system'
+    ],
+    'signInForm' => [
+        'name' => 'Sign In form',
+        'description' => 'Login'
+    ],
+    'registrationForm' => [
+    	'name' => 'Registration form',
+    	'description' => 'Inscription'
+    ],
+    'updateAccountForm' => [
+        'name' => 'Update account form',
+        'description' => 'Update profile'
+    ]
+    
+];
