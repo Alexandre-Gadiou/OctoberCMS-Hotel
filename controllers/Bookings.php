6c37fac0cf7be704b872cd00481f47211c563b15
Alexandre Gadiou <alexandregadiou44@icloud.com>
2017-02-28 16:10:57 +0100

    add backend filter customer by email

--- controllers/Bookings.php
+++ controllers/Bookings.php
@@ -5,6 +5,10 @@ namespace Algad\Hotel\Controllers;
 use BackendMenu;
 use Backend\Classes\Controller;
 use Algad\Hotel\Models\Booking;
+use Algad\Hotel\Models\Customer;
+use DateTime;
+use DateTimeZone;
+use Backend\Models\UserPreference;
 
 /**
  * Bookings Back-end Controller
@@ -21,12 +25,48 @@ class Bookings extends Controller
     public $requiredPermissions = [
         'algad.hotel.manage_bookings'
     ];
+    public $timestamps = true;
 
     public function __construct()
     {
         parent::__construct();
 
         BackendMenu::setContext('Algad.Hotel', 'hotel', 'bookings');
+    }
+
+    public function listOverrideColumnValue($record, $columnName, $definition)
+    {
+        $preferences = UserPreference::forUser()->get('backend::backend.preferences');
+        $timezone = $preferences['timezone'];
+        $datetimeFormat = "Y-m-d H:i:s";
+        $dateFormat = "Y-m-d";
+
+        $result = "";
+        switch ($columnName)
+        {
+            case 'checkin' :
+                $result = $this->getFormatedDate($record->checkin, $timezone, $dateFormat);
+                break;
+            case 'checkout' :
+                $result = $this->getFormatedDate($record->checkout, $timezone, $dateFormat);
+                break;
+            case 'created_at' :
+                $result = $this->getFormatedDate($record->created_at, $timezone, $datetimeFormat);
+                break;
+            case 'updated_at' :
+                $result = $this->getFormatedDate($record->updated_at, $timezone, $datetimeFormat);
+                break;
+            default:
+                break;
+        }
+
+        return $result;
+    }
+
+    private function getFormatedDate($input, $timezone, $format)
+    {
+        $result = (new \DateTime($input))->setTimezone(new \DateTimeZone($timezone));
+        return $result->format($format);
     }
 
     public function index_onDelete()
