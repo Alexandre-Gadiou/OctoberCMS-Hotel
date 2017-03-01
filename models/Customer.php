80d136b4725a6598aa402562120fc66de8d0110a
Alexandre Gadiou <alexandregadiou44@icloud.com>
2017-03-01 12:20:10 +0100

    add Customer validation rule

--- models/Customer.php
+++ models/Customer.php
@@ -11,6 +11,9 @@ class Customer extends User
 {
 
     public $rules = [
+        'name' => 'required|alpha',
+        'surname' => 'required|alpha',
+        'dateOfBirth' => 'required|date',
         'email' => 'required|between:6,255|email|unique:users',
         'password' => 'required:create|between:4,255|confirmed',
         'password_confirmation' => 'required_with:password|between:4,255'
